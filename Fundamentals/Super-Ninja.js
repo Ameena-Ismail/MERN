d643811e475ab5f78e065432a5960b8efe94df6d
Ameena-Ismail <adv.ameena20@hotmail.com>
2021-07-12 11:17:14 +0300

    MERN

--- Fundamentals/Super-Ninja.js
+++ Fundamentals/Super-Ninja.js
@@ -5,25 +5,30 @@ class Ninja {
         this.speed = 3;
         this.strength = 3;
     }
-    sayName = () => console.log(this.name);
-    showStats = () => console.log(`name: ${this.name} strength: ${this.strength} speed: ${this.speed} health: ${this.health}`);
-    drinkSake = () => this.health += 10;
-}
 
+    SayName = () => console.log(`${this.name}`);
+
+    showStats = () => console.log(`Ninja name is: ${this.name} , and his health :${this.health} ,and his speed :${this.speed},and his stength:${this.strength}`);
+
+    drinkSake = () => this.health += 10;
+
+
+}
 
 
 class Sensei extends Ninja {
-    constructor(name) {
-        super(name, 200)
+    constructor(name, health) {
+        super(name, health);
         this.wisdom = 10;
-        this.speed = 10;
-        this.strength = 10;
+
     }
-    speakWisdom = () => {
-        this.drinkSake();
-        console.log("You must treat everyone with respect");
-    }
+
+    speakWisdom = () => console.log("What one programmer can do in one month, two programmers can do in two months");
+    drinkSake = () => console.log(`Name: ${this.name}, Health: ${this.health}, Speed: ${this.speed}, Strength: ${this.strength}`);
+
+
+
 }
-const newSensei = new Sensei("Ameena");
-newSensei.speakWisdom();
-newSensei.showStats();
+const superSensei = new Sensei("Master Splinter", 200);
+superSensei.speakWisdom();
+superSensei.showStats();
