c49d76f42cc3637a99c79c2a2ed1a7c5e158cbaf
Ameena-Ismail <adv.ameena20@hotmail.com>
2021-07-13 11:46:45 +0300

    React

---
+++ React/ameena-first-app/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+
+function App() {
+  return (
+
+    <div >
+      <h1>Hello Dojo !</h1>
+      <div id="list">
+        <h2>Things I need to do :</h2>
+        <ul> 
+          <li>Learn React</li>
+          <li>climb Mt.Everest</li>
+          <li>run a marathon</li>
+          <li>feed a cat</li>
+        </ul>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
